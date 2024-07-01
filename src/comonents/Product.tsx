9c0bdacd3087becfc4780bad98a742455c6acbb3
hassaniqbal23 <hassaniqbalkiu@gmail.com>
2024-07-01 11:29:52 +0500

    fix typescript error

--- src/comonents/Product.tsx
+++ src/comonents/Product.tsx
@@ -15,16 +15,15 @@ export default function Product() {
   console.log("hello world");
   return (
     <div className="container mx-auto flex flex-wrap justify-center mt-5 gap-4">
-      {productQuery.data &&
-        productQuery.data.map((posts) => (
-          <div key={posts.id} className="w-96 h-auto border border-black  p-5">
-            <h2 className="text-lg font-bold mb-2">
-              <span className="border border-black p-2">{posts.id}</span>{" "}
-              {posts.body}
-            </h2>
-            <p className="text-gray-700">{posts.title}</p>
-          </div>
-        ))}
+      {productQuery.data?.map((posts) => (
+        <div key={posts.id} className="w-96 h-auto border border-black  p-5">
+          <h2 className="text-lg font-bold mb-2">
+            <span className="border border-black p-2">{posts.id}</span>{" "}
+            {posts.body}
+          </h2>
+          <p className="text-gray-700">{posts.title}</p>
+        </div>
+      ))}
     </div>
   );
 }
